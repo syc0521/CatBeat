e74180ca9134a88c4462d7b8e762d9b28a482767
MONIAO <2234651603@qq.com>
2020-12-19 09:48:00 +0800

    更改input

---
+++ Assets/Scripts/Play/InputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    Input InputControls;
+    private void Awake()
+    {
+        InputControls = new Input();
+
+        InputControls.PlayController.Tap_Red.performed += ctx => DestroyTapRed();
+    }
+
+    private void DestroyTapRed()
+    {
+
+    }
+
+    void OnEnable()
+    {
+        InputControls.PlayController.Enable();
+    }
+
+}
