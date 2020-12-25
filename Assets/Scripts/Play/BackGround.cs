25e82d8dc0b7cb19c674e97aefecc0d834027d89
syc0521 <65008252+syc0521@users.noreply.github.com>
2020-12-25 09:21:03 +0800

    modify songlist

--- Assets/Scripts/Play/BackGround.cs
+++ Assets/Scripts/Play/BackGround.cs
@@ -12,7 +12,6 @@ public class BackGround : MonoBehaviour
         sprite = GetComponent<SpriteRenderer>().sprite;
         startX = sprite.bounds.extents.x * transform.localScale.x - 0.01f;
         startPos = new Vector3(startX * 2, transform.position.y);
-        Debug.Log(startPos);
     }
 
     void Update()
