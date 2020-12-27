29bfe86ceff6a3095fa641f809b1c126ff13f52d
syc0521 <65008252+syc0521@users.noreply.github.com>
2020-12-27 21:08:32 +0800

    result prototype

--- Assets/Scripts/Result/Result.cs
+++ Assets/Scripts/Result/Result.cs
@@ -8,17 +8,47 @@ public class Result : MonoBehaviour
     public ResultInfo Info;
 
     public TextMesh[] text;
+    public Text[] judgeDetail;
+    public Text comboText;
+    public Text nameText;
+    public Text diffText;
 
     private void Start()
     {
         Info.score = 55555;//测试用代码
-        StartCoroutine(showScore());
+        StartCoroutine(ShowScore());
+        ShowJudge();
+    }
+
+    private void ShowJudge()
+    {
+        judgeDetail[0].text = NoteController.perfect.ToString();
+        judgeDetail[1].text = NoteController.great.ToString();
+        judgeDetail[2].text = NoteController.good.ToString();
+        judgeDetail[3].text = NoteController.miss.ToString();
+        comboText.text = NoteController.combo.ToString();
+        diffText.text = ShowLevel();
+    }
+
+    private string ShowLevel()
+    {
+        var song = SongManager.songList.Find(item => item.Path == NoteController.path);
+        switch (NoteController.diff)
+        {
+            case Diff.Easy:
+                return "Easy Lv." + song.EasyLevel;
+            case Diff.Normal:
+                return "Normal Lv." + song.NormalLevel;
+            case Diff.Hard:
+                return "Hard Lv." + song.HardLevel;
+        }
+        return null;
     }
     /// <summary>
     /// 展示分数
     /// </summary>
     /// <returns></returns>
-    IEnumerator showScore()
+    IEnumerator ShowScore()
     {
         yield return new WaitForSeconds(2f);
 
@@ -47,7 +77,7 @@ public class Result : MonoBehaviour
             yield return new WaitForSeconds(0.01f);
             for (int j = 0; j < 5; j++)
             {
-                text[j].text = getNum(i, j);
+                text[j].text = GetNum(i, j);
             }
             Debug.Log(i);
 
@@ -66,9 +96,9 @@ public class Result : MonoBehaviour
     /// <param name="k"></param>
     /// <param name="val"></param>
     /// <returns></returns>
-    private string getNum(int val, int k)
+    private string GetNum(int val, int k)
     {
-        val = val / (int)Mathf.Pow(10, k);
+        val /= (int)Mathf.Pow(10, k);
         return (val % 10).ToString();
     }
 
