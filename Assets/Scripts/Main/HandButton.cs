472102b52e81c63bcb7111ec21b98276f66bd9d9
syc0521 <65008252+syc0521@users.noreply.github.com>
2021-01-04 00:11:54 +0800

    add secret sound

--- Assets/Scripts/Main/HandButton.cs
+++ Assets/Scripts/Main/HandButton.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class HandButton : MonoBehaviour
 {
+    public AudioSource getSound;
     private int count = 0;
     private void OnMouseDown()
     {
-        Debug.Log(MainSceneManager.ending);
         if (MainSceneManager.ending && !MainSceneManager.secret && count < 5)
         {
             count++;
-        }
-        if (count == 5)
-        {
-            Debug.Log("secret unlocked");
-            MainSceneManager.secret = true;
-            Utils.save.SystemSettings = new SaveData.Settings
+            if (count == 5)
             {
-                isAutoPlay = Utils.save.SystemSettings.isAutoPlay,
-                speed = Utils.save.SystemSettings.speed,
-                hitVol = Utils.save.SystemSettings.hitVol,
-                ending = MainSceneManager.ending,
-                secret = MainSceneManager.secret
-            };
-            Utils.SavePrefs();
+                getSound.Play();
+                Debug.Log("secret unlocked");
+                MainSceneManager.secret = true;
+                Utils.save.SystemSettings = new SaveData.Settings
+                {
+                    isAutoPlay = Utils.save.SystemSettings.isAutoPlay,
+                    speed = Utils.save.SystemSettings.speed,
+                    hitVol = Utils.save.SystemSettings.hitVol,
+                    ending = MainSceneManager.ending,
+                    secret = MainSceneManager.secret
+                };
+                Utils.SavePrefs();
+            }
         }
     }
 }
